7128c0ab531e9958e1c26554aa8bbc22fd0670e6
christopher-howard <chrisau233@gmail.com>
2018-06-05 21:39:07 +1000

    remove comments

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import { StripeProvider } from "react-stripe-elements";
-
-// import MyStoreCheckout from "./MyStoreCheckout";
 
 const styles = theme => ({});
 class HomePage extends Component {
@@ -11,12 +8,7 @@ class HomePage extends Component {
     this.state = {};
   }
   render() {
-    return (
-      <div>test</div>
-      // <StripeProvider apiKey="pk_test_12345">
-      //   <MyStoreCheckout />
-      // </StripeProvider>
-    );
+    return <div>HOME PAGE</div>;
   }
 }
 export default withStyles(styles)(HomePage);
