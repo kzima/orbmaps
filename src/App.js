677a4b9a22ed1026c6820c1ad04ff430f5046c22
christopher-howard <chrisau233@gmail.com>
2018-06-05 21:36:25 +1000

    add pages/approute/layout comps

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
-import TopBar from "./Components/TopBar";
+import TopBarLayout from "./Components/TopBarLayout";
+import NoTopBarLayout from "./Components/NoTopBarLayout";
 import HomePage from "./Components/HomePage";
-import AboutPage from "./Components/HomePage";
-import ContactPage from "./Components/HomePage";
+import AboutPage from "./Components/AboutPage";
+import ContactPage from "./Components/ContactPage";
+import RegisterPage from "./Components/RegisterPage";
+import AppRoute from "./Components/AppRoute";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TopBar />
         <main>
           <Switch>
-            <Route path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/contact" component={ContactPage} />
+            <AppRoute
+              exact
+              path="/"
+              layout={TopBarLayout}
+              component={HomePage}
+            />
+            <AppRoute
+              exact
+              path="/about"
+              layout={TopBarLayout}
+              component={AboutPage}
+            />
+            <AppRoute
+              exact
+              path="/contact"
+              layout={TopBarLayout}
+              component={ContactPage}
+            />
+            <AppRoute
+              exact
+              path="/register"
+              layout={NoTopBarLayout}
+              component={RegisterPage}
+            />
           </Switch>
         </main>
       </div>
