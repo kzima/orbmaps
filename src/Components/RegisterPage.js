09605ce73faca57f0d1c3903e6ce8cae2a94d43b
christopher-howard <chrisau233@gmail.com>
2018-06-12 10:14:39 +1000

    finalise styling #2

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -2,12 +2,13 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 
-import Image from "../register-bg.png";
+import Image from "../bg.png";
 import RegisterForm from "./RegisterForm";
 
 const styles = theme => ({
   root: {
-    backgroundImage: `url(${Image})`
+    backgroundImage: `url(${Image})`,
+    backgroundSize: "cover"
   },
   button: {
     marginBottom: theme.spacing.unit * 4
@@ -18,7 +19,11 @@ function RegisterPage(props) {
   return (
     <div className={props.classes.root}>
       <RegisterForm />
-      <Button className={props.classes.button} href="https://www.orbmaps.com">
+      <Button
+        variant="contained"
+        className={props.classes.button}
+        href="https://www.orbmaps.com"
+      >
         Go Back
       </Button>
     </div>
