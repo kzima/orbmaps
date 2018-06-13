d404f0c5a32b06f11abae671f9cbd251ff199474
christopher-howard <chrisau233@gmail.com>
2018-06-13 16:56:37 +1000

    add t&c and pricing

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -56,8 +56,14 @@ const styles = theme => ({
     marginBottom: 20
   },
   price: {
-    marginTop: 30,
+    marginTop: 35,
     marginBottom: 10
+  },
+  terms: {
+    maxWidth: 400,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 30
   }
 });
 
@@ -118,7 +124,6 @@ class RegisterForm extends React.Component {
     this.validatePassword();
   };
 
-  // REVIEW:
   handleSubmit = event => {
     event.preventDefault();
     const contact =
@@ -128,13 +133,6 @@ class RegisterForm extends React.Component {
       processing: true,
       error: ""
     });
-    // this.props.stripe
-    //   .createToken({ name: this.state.data.name })
-    //   .then(({ token }) => {
-    //     this.setState({
-    //       token: token
-    //     });
-    //   });
 
     fetch(registerURL, {
       method: "POST",
@@ -314,14 +312,23 @@ class RegisterForm extends React.Component {
                         color="primary"
                         gutterBottom
                       >
-                        Presale Plan: 1 License - $195.00
+                        Presale Plan: 1 License ($195.00)
                       </Typography>
                     </Grid>
                     <Grid item>
                       <CardSection />
                     </Grid>
                   </Grid>
-
+                  <Typography className={classes.terms} variant="caption">
+                    By registering, I confirm that I understand and agree to the{" "}
+                    <a href="https://www.orbmaps.com/terms-conditions">
+                      Terms & Conditions
+                    </a>{" "}
+                    and{" "}
+                    <a href="https://www.orbmaps.com/privacy-policy">
+                      Privacy Policy
+                    </a>.
+                  </Typography>
                   <Button
                     className={classes.registerButton}
                     variant="contained"
